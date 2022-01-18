301edd7c4de7482f5d8a985af62ae2022f25969d
litecave <64094645+litecave@users.noreply.github.com>
2022-01-18 16:59:15 +0300

    Update Kernel.cs

--- kocmoc/Kernel.cs
+++ kocmoc/Kernel.cs
@@ -1,25 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using kocmoc.ErrorHandle;
 using kocmoc.Programs;
+using Cosmos.System.Graphics;
+using Cosmos.HAL;
+using Cosmos.System.FileSystem;
 using Sys = Cosmos.System;
 
 namespace kocmoc
 {
     public class Kernel : Sys.Kernel
     {
+        public static VGAScreen VScreen = new VGAScreen();
         string UserName = "Default";
-        Sys.FileSystem.CosmosVFS fs;
+        Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
         string current_directory = "0:\\";
         string whdx = String.Empty;
+        
 
         protected override void BeforeRun()
         {
-            Sys.FileSystem.CosmosVFS fs;
+            Console.WriteLine("Vga Driver Booting");
+            VGAScreen.SetGraphicsMode(VGADriver.ScreenSize.Size720x480, ColorDepth.ColorDepth32);
+            Console.WriteLine("Vga Driver Booted");
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            Directory.CreateDirectory("fank");
+            File.Create("0:\\re.txt");
             string current_directory = "0:\\";
             Console.WriteLine("DUMBASSOS");
+            
 
 
         }
@@ -68,6 +79,51 @@ namespace kocmoc
                 Console.WriteLine("cun - Change UserName");
                 Console.WriteLine("sysver - about system");
                 Console.WriteLine("utils - small dev utils");
+                Console.WriteLine("-=Files Help=-");
+                Console.WriteLine("ls - list of files");
+                Console.WriteLine("mkdir [foldername] - makes folder");
+            }
+            else if (input.StartsWith("mkdir "))
+            {
+                Directory.CreateDirectory(current_directory + input.Remove(0, 6));
+            }
+            else if (input.StartsWith("cd "))
+            {
+                bool isDirExist = Directory.Exists(current_directory + input.Remove(0, 3));
+                try
+                {
+                    if (isDirExist == true)
+                    {
+                        current_directory = current_directory + input.Remove(0, 3);
+                    }
+                    else if (input.Remove(0, 3) == "0:\\")
+                    {
+                        current_directory = "0:\\";
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            else if (input.StartsWith("touch "))
+            {
+                string path = current_directory + input.Remove(0, 6);
+                File.Create(path);
+            }
+            else if (input == "shutdown")
+            {
+                Sys.Power.Shutdown();
+            }
+            else if (input == "reboot")
+            {
+                Sys.Power.Reboot();
+            }
+            else if (input.StartsWith("cat "))
+            {
+                string file = input.Remove(0, 4);
+                var content = File.ReadAllText(file);
+                Console.WriteLine("File Name: {0} \nFile size: {1} \nFile Content: \n{2}", file, content.Length, content);
             }
         }
 
